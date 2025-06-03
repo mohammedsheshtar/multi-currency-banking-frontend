912dbb95d351d1721d53da7945e1c277e56812be
Njjoodd <nejood03@gmail.com>
2025-06-04 02:34:49 +0300

    back arrow or whatever

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,6 +57,7 @@ dependencies {
     implementation("androidx.activity:activity-compose:1.10.1")
     implementation(platform("androidx.compose:compose-bom:2025.05.01"))
     implementation("androidx.compose.ui:ui")
+    implementation("androidx.compose.material3:material3:1.2.0")
     val navVersion = "2.7.6"
     implementation("androidx.navigation:navigation-compose:$navVersion")
     implementation("androidx.compose.material3:material3:1.2.0")
--- app/src/main/java/com/joincoded/bankapi/composable/RegistrationScreen.kt
+++ app/src/main/java/com/joincoded/bankapi/composable/RegistrationScreen.kt
@@ -6,6 +6,8 @@ import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicText
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
@@ -26,6 +28,8 @@ import com.joincoded.bankapi.data.request.CreateUserDTO
 import com.joincoded.bankapi.data.request.KYCRequest
 import com.joincoded.bankapi.viewmodel.AuthViewModel
 import java.math.BigDecimal
+import androidx.compose.material3.TopAppBarDefaults
+
 
 @Composable
 fun RegistrationScreen(navController: NavController) {
@@ -63,40 +67,53 @@ fun RegistrationScreen(navController: NavController) {
         unfocusedIndicatorColor = Color.Transparent,
         cursorColor = primaryDark
     )
+    @OptIn(ExperimentalMaterial3Api::class)
 
-    Box(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(backgroundDark)
-            .padding(22.dp)
-    ) {
-        Column(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(top = 50.dp),
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            BasicText(
-                text = "Create your account!",
-                style = TextStyle(brush = gradientBrush, fontSize = 26.sp, fontWeight = FontWeight.Bold)
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {},
+                navigationIcon = {
+                    IconButton(onClick = { navController.popBackStack() }) {
+                        Icon(
+                            imageVector = Icons.Default.ArrowBack,
+                            contentDescription = "Back",
+                            tint = Color.White
+                        )
+                    }
+                },
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = Color.Transparent,
+                    titleContentColor = Color.White
+                )
             )
-        }
-
+        },
+        containerColor = backgroundDark
+    ) { innerPadding ->
         Column(
             modifier = Modifier
                 .fillMaxSize()
+                .padding(innerPadding)
                 .padding(horizontal = 24.dp)
-            .verticalScroll(rememberScrollState()),
+                .verticalScroll(rememberScrollState()),
             horizontalAlignment = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.Center
         ) {
+            BasicText(
+                text = "Create your account!",
+                style = TextStyle(brush = gradientBrush, fontSize = 26.sp, fontWeight = FontWeight.Bold),
+                modifier = Modifier.padding(bottom = 24.dp)
+            )
+
             if (!proceedToKyc) {
                 TextField(
                     value = firstName,
                     onValueChange = { firstName = it },
                     label = { Text("First Name", color = Color.White) },
                     colors = fieldColors,
-                    modifier = Modifier.fillMaxWidth().clip(RoundedCornerShape(30.dp))
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .clip(RoundedCornerShape(30.dp))
                         .padding(vertical = 4.dp)
                 )
                 TextField(
@@ -104,7 +121,9 @@ fun RegistrationScreen(navController: NavController) {
                     onValueChange = { lastName = it },
                     label = { Text("Last Name", color = Color.White) },
                     colors = fieldColors,
-                    modifier = Modifier.fillMaxWidth().clip(RoundedCornerShape(30.dp))
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .clip(RoundedCornerShape(30.dp))
                         .padding(vertical = 4.dp)
                 )
                 TextField(
@@ -112,7 +131,9 @@ fun RegistrationScreen(navController: NavController) {
                     onValueChange = { username = it },
                     label = { Text("Username", color = Color.White) },
                     colors = fieldColors,
-                    modifier = Modifier.fillMaxWidth().clip(RoundedCornerShape(30.dp))
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .clip(RoundedCornerShape(30.dp))
                         .padding(vertical = 4.dp)
                 )
                 TextField(
@@ -121,7 +142,9 @@ fun RegistrationScreen(navController: NavController) {
                     label = { Text("Password", color = Color.White) },
                     visualTransformation = PasswordVisualTransformation(),
                     colors = fieldColors,
-                    modifier = Modifier.fillMaxWidth().clip(RoundedCornerShape(30.dp))
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .clip(RoundedCornerShape(30.dp))
                         .padding(vertical = 4.dp)
                 )
                 TextField(
@@ -130,7 +153,9 @@ fun RegistrationScreen(navController: NavController) {
                     label = { Text("Confirm Password", color = Color.White) },
                     visualTransformation = PasswordVisualTransformation(),
                     colors = fieldColors,
-                    modifier = Modifier.fillMaxWidth().clip(RoundedCornerShape(30.dp))
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .clip(RoundedCornerShape(30.dp))
                         .padding(vertical = 4.dp)
                 )
 
@@ -141,7 +166,6 @@ fun RegistrationScreen(navController: NavController) {
                         } else {
                             authViewModel.setAuthMessage("Passwords do not match.")
                         }
-
                     },
                     modifier = Modifier.fillMaxWidth(),
                     colors = ButtonDefaults.buttonColors(containerColor = Color.White)
@@ -155,7 +179,9 @@ fun RegistrationScreen(navController: NavController) {
                     onValueChange = { dateOfBirth = it },
                     label = { Text("Date of Birth (YYYY-MM-DD)", color = Color.White) },
                     colors = fieldColors,
-                    modifier = Modifier.fillMaxWidth().clip(RoundedCornerShape(30.dp))
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .clip(RoundedCornerShape(30.dp))
                         .padding(vertical = 4.dp)
                 )
                 TextField(
@@ -163,7 +189,9 @@ fun RegistrationScreen(navController: NavController) {
                     onValueChange = { civilId = it },
                     label = { Text("Civil ID", color = Color.White) },
                     colors = fieldColors,
-                    modifier = Modifier.fillMaxWidth().clip(RoundedCornerShape(30.dp))
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .clip(RoundedCornerShape(30.dp))
                         .padding(vertical = 4.dp)
                 )
                 TextField(
@@ -171,7 +199,9 @@ fun RegistrationScreen(navController: NavController) {
                     onValueChange = { country = it },
                     label = { Text("Country", color = Color.White) },
                     colors = fieldColors,
-                    modifier = Modifier.fillMaxWidth().clip(RoundedCornerShape(30.dp))
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .clip(RoundedCornerShape(30.dp))
                         .padding(vertical = 4.dp)
                 )
                 TextField(
@@ -179,7 +209,9 @@ fun RegistrationScreen(navController: NavController) {
                     onValueChange = { phoneNumber = it },
                     label = { Text("Phone Number", color = Color.White) },
                     colors = fieldColors,
-                    modifier = Modifier.fillMaxWidth().clip(RoundedCornerShape(30.dp))
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .clip(RoundedCornerShape(30.dp))
                         .padding(vertical = 4.dp)
                 )
                 TextField(
@@ -187,7 +219,9 @@ fun RegistrationScreen(navController: NavController) {
                     onValueChange = { homeAddress = it },
                     label = { Text("Home Address", color = Color.White) },
                     colors = fieldColors,
-                    modifier = Modifier.fillMaxWidth().clip(RoundedCornerShape(30.dp))
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .clip(RoundedCornerShape(30.dp))
                         .padding(vertical = 4.dp)
                 )
                 TextField(
@@ -195,7 +229,9 @@ fun RegistrationScreen(navController: NavController) {
                     onValueChange = { salary = it },
                     label = { Text("Salary (in KD)", color = Color.White) },
                     colors = fieldColors,
-                    modifier = Modifier.fillMaxWidth().clip(RoundedCornerShape(30.dp))
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .clip(RoundedCornerShape(30.dp))
                         .padding(vertical = 4.dp)
                 )
 
@@ -231,7 +267,6 @@ fun RegistrationScreen(navController: NavController) {
             if (authMessage == "Registration and KYC completed successfully!") {
                 LaunchedEffect(Unit) {
                     navController.navigate(NavRoutes.NAV_ROUTE_LOGIN_SCREEN.value + "?registered=true")
-
                 }
             }
         }
